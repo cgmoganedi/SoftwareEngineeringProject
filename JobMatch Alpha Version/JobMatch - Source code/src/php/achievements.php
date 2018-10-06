a51896d372bf10e141e1bb18f4af96e855bb34e0
OpSys <1137682@students.wits.ac.za>
2018-10-07 01:49:58 +0200

    Add files via upload

---
+++ JobMatch Alpha Version/JobMatch - Source code/src/php/achievements.php
@@ -0,0 +1,48 @@
+<?php
+include_once 'connection.php';
+include_once 'things.php';
+session_start();
+$ID = $_SESSION['UserID'];
+$usr_achievement = $_POST['achievement'];
+$usr_achievement_desc = $_POST['achievement_desc'];
+$usr_achievement_yr = $_POST['achievement_yr'];
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//check if data already exists:
+$sql = "SELECT * FROM user_achievements where ID = '$ID'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0)
+{
+    $sql = "UPDATE user_achievements SET Achievement_name = '$usr_achievement', Achievement_Description = '$usr_achievement_desc', Year_Obtained = '$usr_achievement_yr' WHERE user_achievements.ID = '$ID'";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        header('Location: /JobMatch/src/php/welcome.php');
+        exit();
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+else{
+    $sql = "INSERT INTO user_achievements (ID,Achievement_name,Achievement_Description) VALUES ('$ID','$usr_achievement','$usr_achievement_desc','$usr_achievement_yr')";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        header('Location: /JobMatch/src/php/welcome.php');
+        exit();
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+
+
+
---
+++ JobMatch Alpha Version/JobMatch - Source code/src/php/skills.php
@@ -0,0 +1,46 @@
+<?php
+include_once 'connection.php';
+include_once 'things.php';
+session_start();
+$ID = $_SESSION['UserID'];
+$usr_skill = $_POST['usr_skill'];
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//check if data already exists:
+$sql = "SELECT * FROM user_skills where ID = '$ID'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0)
+{
+    $sql = "UPDATE user_skills SET Skill = '$usr_skill' WHERE user_skills.ID = '$ID'";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        header('Location: /JobMatch/src/php/welcome.php');
+        exit();
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+else{
+    $sql = "INSERT INTO user_skills (ID,Skill) VALUES ('$ID','$usr_skill')";
+
+    if ($conn->query($sql) === TRUE) {
+        $conn->close();
+        header('Location: /JobMatch/src/php/welcome.php');
+        exit();
+    }
+    else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+
+
+
