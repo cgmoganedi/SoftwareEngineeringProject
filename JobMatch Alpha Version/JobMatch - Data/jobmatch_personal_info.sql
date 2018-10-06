9af48ec4a60249dbbc79f8b6f0a5cf54b6a744f5
OpSys <1137682@students.wits.ac.za>
2018-10-07 00:03:56 +0200

    Delete jobmatch_personal_info.sql

--- JobMatch Alpha Version/JobMatch - Data/jobmatch_personal_info.sql
+++
@@ -1,1 +0,0 @@
-UPDATE jobmatch.personal_info SET Title = 'Mr', Name = 'John', Surname = 'Carter', Country = 'South Africa', Date_of_Birth = '05-April-2018', Id_Num = '1234567890123', Email = '', Marital_Status = '', Gender = 'Male', Race = 'Indian', City = 'Jozi' WHERE ID = 6;
