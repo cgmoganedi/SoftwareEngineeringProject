f08e832ec01624385065cae82ed4927b52208040
OpSys <1137682@students.wits.ac.za>
2018-10-07 00:04:04 +0200

    Delete jobmatch_users.sql

--- JobMatch Alpha Version/JobMatch - Data/jobmatch_users.sql
+++
@@ -1,2 +0,0 @@
-UPDATE jobmatch.users SET Email = 'john@gmail.com', Password = 'man', Name = 'John' WHERE ID = 6;
-UPDATE jobmatch.users SET Email = 'john1@gmail.com', Password = 'man', Name = 'JC' WHERE ID = 7;
