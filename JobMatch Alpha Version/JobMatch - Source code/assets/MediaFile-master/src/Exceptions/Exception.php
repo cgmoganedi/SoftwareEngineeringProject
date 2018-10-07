1c0a648d07e69552b9333f78e2da47bfcb4bdd47
OpSys <1137682@students.wits.ac.za>
2018-10-07 20:08:38 +0200

    Deleted JobMatch Alpha Version

--- JobMatch Alpha Version/JobMatch - Source code/assets/MediaFile-master/src/Exceptions/Exception.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace wapmorgan\MediaFile\Exceptions;
-
-class Exception extends \Exception {}
--- JobMatch Alpha Version/JobMatch - Source code/assets/MediaFile-master/src/Exceptions/FileAccessException.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace wapmorgan\MediaFile\Exceptions;
-
-class FileAccessException extends Exception {}
